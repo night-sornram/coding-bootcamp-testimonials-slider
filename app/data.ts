9b3d0c2d0465dbb8162013d4a683c761cf4fb611
night-sornram <sornram.chalernma@gmail.com>
2024-01-11 10:07:22 +0700

    add

---
+++ app/data.ts
@@ -0,0 +1,21 @@
+interface Data {
+    quotes : string,
+    name : string,
+    job : string
+}
+
+type Datas = Data[];
+
+ export  const data : Datas =[
+    {
+        quotes : '“ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ”',
+        name : "Tanya Sinclair",
+        job : "UX Engineer"
+    },
+    {
+        quotes : '“ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ”',
+        name : "John Tarkpor ",
+        job : "Junior Front-end Developer"
+    },
+
+]
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,29 @@ const config: Config = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'bg' : "url('/images/pattern-bg.svg')",
+        'quotes' : "url('/images/pattern-quotes.svg')"
       },
+      backgroundPosition :{
+        bottom: 'bottom',
+        center: 'center',
+        left: 'left',
+        'left-bottom': 'left bottom',
+        'left-top': 'left top',
+        right: 'right',
+        'right-bottom': 'right bottom',
+        'right-top': 'right top',
+        top: 'top',
+        "custom" : 'top left 8rem'
+
+      },
+      backgroundSize: {
+        'auto': 'auto',
+        'cover': 'cover',
+        'contain': 'contain',
+        "customs" : '7rem',
+        "customsm" : '4rem'
+      }
     },
   },
   plugins: [],
